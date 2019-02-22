ae68bdcd4d65973e15c2ba8e71008573dbe7832d
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-02-22 10:15:58 +0300

    added a modified html file on the calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business( or back-end)logic:
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -10,3 +11,13 @@ var multiply = function(number1, number2) {
 var divide = function(number1, number2) {
   return number1 / number2;
 };
+//user interface(or front-end)logic:
+$(document).ready(function() {
+$("form#add").submit(function(event) {
+event.preventDefault();
+var number1 = parseInt($("#add1").val());
+var number2 = parseInt($("#add2").val());
+var result = add(number1, number2);
+$("#output").text(result);
+});
+});
